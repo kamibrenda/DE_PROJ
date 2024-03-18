4bffbca74d504514654f5dabaaa1f3d4e7933acf
kamibrenda <mukamibrenda254@gmail.com>
2024-03-18 16:37:58 +0300

    Pipeline built

--- 2_docker_sql/pipeline.py
+++ 2_docker_sql/pipeline.py
@@ -1,11 +1,22 @@
-import sys 
+services:
+  postgres:
+    image: postgres:13
+    environment:
+      POSTGRES_USER: airflow
+      POSTGRES_PASSWORD: airflow
+      POSTGRES_DB: airflow
+    volumes: 
+      - postgres-db-volume:/var/lib/postgresql/data
+    healthcheck:
+      test: ["CMD", "pg_isready", "-U", "airflow"]
+      interval: 5s
+      retries: 5
+    restart: always
 
-import pandas as pd
-
-print(sys.argv)
-
-day = sys.argv[1]
-
-#some fancy code
-
-print(f'Job finished successfully for day = f{day}')
+docker run -it \
+   -e POSTGRES_USER = "root" \
+   -e POSTGRES_PASSWORD= "root" \
+   -e POSTGRES_DB="ny_taxi"  \
+   -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data 
+   -p 5432:5432 \
+   postgres:13
