7072869c26a1c55c0c6854de0589d6edc97ff72a
kamibrenda <mukamibrenda254@gmail.com>
2024-03-18 15:23:38 +0300

    Ingestion

---
+++ 2_docker_sql/READme.md
@@ -0,0 +1,4 @@
+sys.argv[1] 
+Arguement 0 is the nane of the file and 1 is what we pass
+
+day = sys.argv[1]
---
+++ 2_docker_sql/pipeline.py
@@ -0,0 +1,11 @@
+import sys 
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+#some fancy code
+
+print(f'Job finished successfully for day = f{day}')
