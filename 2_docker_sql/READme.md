03ad850813ebff39f56168500c9c1f84e781c036
kamibrenda <mukamibrenda254@gmail.com>
2024-04-16 16:39:06 +0300

    Update

--- 2_docker_sql/READme.md
+++
@@ -1,4 +0,0 @@
-sys.argv[1] 
-Arguement 0 is the nane of the file and 1 is what we pass
-
-day = sys.argv[1]
---
+++ 2_docker_sql/pipeline.py
@@ -0,0 +1,11 @@
+import sys 
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+# some fancy code
+
+print('Job finished successfully for day = f{day}')
